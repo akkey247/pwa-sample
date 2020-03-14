35e5d07556af704cdf3d3492405e3691ad537a63
akkey <akkey52417@gmail.com>
2020-03-14 22:27:53 +0900

    ちょっとリファクタ

--- service_worker.js
+++ service_worker.js
@@ -1,9 +1,7 @@
-// ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
-
 // キャッシュ名とキャッシュファイルの指定
 var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
-	'/poster-keisuke.github.io/',
+    '/akkey247.github.io/',
 ];
 
 // インストール処理
